5f1bcaef439ff9474ff66bab0cb4a63e9b18a152
pallavi60760 <pallavishrotriya121@gmail.com>
2024-10-18 00:09:55 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,46 @@
+let menuIcon=document.querySelector("#menu-icon");
+let navbar=document.querySelector(".navbar");
+
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+let main=document.querySelectorAll("main");
+let navLinks=document.querySelectorAll("header nav a");
+window.onscroll=()=>{
+    main.forEach(sec => {
+        let top=window.scrollY;
+        let offset=sec.offsetTop-150;
+        let height=sec.offsetHeight;
+        let id=sec.getAttribute("id");
+
+        if (top>=offset && top<offset+height){
+            navLinks.foreach.apply(links=>{
+                links.classList.remove('active');
+                document.querySelector("header nav a[href*="+id+"]").classList.add("active");
+            })
+        }
+    })
+    let header=document.querySelector("header");
+    header.classList.toggle('sticky',window.scrollY>100);
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active')
+};
+ScrollReveal({ 
+    distance:'80px',
+    duration:2000,
+    delay:200,
+ });
+ ScrollReveal().reveal('.home-content , heading', { origin:'top' });
+ ScrollReveal().reveal('.home-img, .service-container, .portfolio-box, .contact form', { origin:'bottom' });
+ ScrollReveal().reveal('.home-contact h1, .about-img', {origin:'left' });
+ ScrollReveal().reveal('.home-content p, .about-content', { origin:'right' });
+ 
+ const typed= new Typed('.multiple-text',{
+    strings:['Full Stack Developer','Frontend Developer', 'Backend Developer', 'Web Designer'],
+    typeSpeed:70,
+    backSpeed:70,
+    backDelay:1000,
+    loop:true,
+
+ });
